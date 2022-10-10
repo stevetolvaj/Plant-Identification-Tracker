e11c693f583beb968c5ba9623323c9e6d904faf6
Steve Tolvaj <Steve Tolvaj@LAPTOP-D52TNI4S>
2022-10-10 17:16:09 -0400

    feature: Added files for click sort tables of users and plants

---
+++ web/js/content/plantTableContent.js
@@ -0,0 +1,11 @@
+"use strict";
+
+function plantTableContent() {
+    var content = `
+        <h2>User List<h2/>
+    
+    `;
+    var ele = document.createElement("div");
+    ele.innerHTML = content;
+    var 
+}
