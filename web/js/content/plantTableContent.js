ec1726357257c3041c5fbe073f4d61495da51092
Steve Tolvaj <Steve Tolvaj@LAPTOP-D52TNI4S>
2022-10-11 10:25:29 -0400

    feature: Completed basic click sort functionality -Plant table data with sort by ascending complete -User table data with sort by ascending complete

--- web/js/content/plantTableContent.js
+++ web/js/content/plantTableContent.js
@@ -1,11 +1,43 @@
 "use strict";
 
+
 function plantTableContent() {
-    var content = `
-        <h2>User List<h2/>
+  var myDiv = document.createElement("div");
+
+  // REPLACE THIS funList assignment statement with an AJAX call to file json/water.json
+  ajax("./webAPIs/listPlantsAPI.jsp", processPlantData, myDiv);
+
+  function processPlantData(plantsList) {
+    console.log("plant list test print:");
+    console.log(plantsList);
     
-    `;
-    var ele = document.createElement("div");
-    ele.innerHTML = content;
-    var 
+    if (plantsList.dbError) {
+        alert("error getting plants from database");
+    }
+    
+    let plants = plantsList.plantList;
+
+            var newPlantList = []; // empty array
+            
+            for (var i = 0; i < plants.length; i++) {
+                newPlantList[i] = {}; // i-th element of array is empty object.
+                newPlantList[i].PlantID = SortableTableUtils.makeNumber(plants[i].plantId);
+                newPlantList[i].PlantName = SortableTableUtils.makeText(plants[i].plantName),
+                newPlantList[i]._Image = SortableTableUtils.makeImage(plants[i].plantImage, "10rem");
+                newPlantList[i].Region = SortableTableUtils.makeText(plants[i].plantRegion);
+                newPlantList[i].UserEmail = SortableTableUtils.makeText(plants[i].webUserEmail);
+
+            }
+
+               function inject(ele, where) {
+                where.innerHTML = ""; // blank out content area before appending
+                where.appendChild(ele);
+            }
+
+            // document.getElementById("listHere").appendChild(MakeSortableTable(carList, "condition"));
+
+            inject(MakeClickSortTable("Plants Identified by Users", newPlantList, "PlantID", "icons/down.png"), myDiv);
+  }
+
+  return myDiv;
 }
--- web/js/content/userTableContent.js
+++ web/js/content/userTableContent.js
@@ -1,7 +1,45 @@
 "use strict";
 
 function userTableContent() {
+    var myDiv = document.createElement("div");
+
+  // REPLACE THIS funList assignment statement with an AJAX call to file json/water.json
+  ajax("./webAPIs/listUsersAPI.jsp", processUserData, myDiv);
+
+  function processUserData(usersList) {
+    console.log("user list test print:");
+    console.log(usersList);
     
+    if (usersList.dbError) {
+        alert("error getting plants from database");
+    }
+    
+    let users = usersList.webUserList;
+
+            var newUsersList = []; // empty array
+            
+            for (var i = 0; i < users.length; i++) {
+                newUsersList[i] = {}; // i-th element of array is empty object.
+                newUsersList[i].UserID = SortableTableUtils.makeNumber(users[i].webUserId);
+                newUsersList[i].Email = SortableTableUtils.makeText(users[i].userEmail),
+                newUsersList[i]._Image = SortableTableUtils.makeImage(users[i].image, "10rem");
+                newUsersList[i].DOB = SortableTableUtils.makeDate(users[i].birthday);
+                newUsersList[i].Role = SortableTableUtils.makeText(users[i].userRoleType);
+
+            }
+
+               function inject(ele, where) {
+                where.innerHTML = ""; // blank out content area before appending
+                where.appendChild(ele);
+            }
+
+            // document.getElementById("listHere").appendChild(MakeSortableTable(carList, "condition"));
+
+            inject(MakeClickSortTable("Registered Users List", newUsersList, "UserID", "icons/down.png"), myDiv);
+  }
+
+  return myDiv;
 }
+
 
 
